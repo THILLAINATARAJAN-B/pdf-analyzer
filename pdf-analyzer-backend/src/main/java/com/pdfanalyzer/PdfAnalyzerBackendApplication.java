d38bdc2675cb4ffb13cb54be06ee2a984ebbfe7b
Thillainatarajan B <thillainatarajan001@gmail.com>
2026-06-04 10:24:07 +0530

    initial setup

---
+++ pdf-analyzer-backend/src/main/java/com/pdfanalyzer/PdfAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pdfanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAnalyzerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfAnalyzerBackendApplication.class, args);
+	}
+
+}
---
+++ pdf-analyzer-backend/src/test/java/com/pdfanalyzer/PdfAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfAnalyzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
